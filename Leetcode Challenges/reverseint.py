4aecc11a61057346019af5f952901efa2a3fab41
Ricardo Leite <ricardo.leitejr95@gmail.com>
2020-12-19 09:30:12 -0700

    Finished reverse int challenge

--- Leetcode Challenges/reverseint.py
+++ Leetcode Challenges/reverseint.py
@@ -2,3 +2,12 @@
 
 # Note:
 # Assume we are dealing with an environment that could only store integers within the 32-bit signed integer range: [−231,  231 − 1]. For the purpose of this problem, assume that your function returns 0 when the reversed integer overflows.
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        sign = [1, -1][x < 0]
+        rev, x = 0, abs(x)
+        while x:
+            x, mod = divmod(x, 10)
+            rev = rev*10 + mod
+        return sign*rev if -pow(2, 31) <= sign*rev <= pow(2, 31)-1 else 0
